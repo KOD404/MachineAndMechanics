2a76a65e32bd75ddd81f699dd6ef5ade80b546d8
Nikita <fatyanov.nikita@yandex.ru>
2023-04-15 09:19:27 +0300

    Add Classes

--- ProjOOP/ProjOOP.cpp
+++ ProjOOP/ProjOOP.cpp
@@ -2,6 +2,30 @@
 
 using namespace std;
 
+static int GetTime = 0;
+
+class Cost {
+private:
+	int GetCostIsTime = 25 * GetTime;
+	int GetCostIsDetails = 5 * GetTime;
+public:
+
+};
+
+class Mechanics {
+private:
+
+public:
+
+};
+
+class Machine {
+private:
+
+public:
+
+};
+
 int main()
 {
 	
